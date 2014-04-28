723fb13db0e079efe45c8f90f127ef10552fa9f2
Rahman Lavaee <r.lavaee@gmail.com>
2014-04-27 21:05:31 -0400

    pdq_analysis changed

--- pdq/pdq_analysis.r
+++ pdq/pdq_analysis.r
@@ -3,9 +3,12 @@ dir <- args[1]
 Interval <- strtoi(args[2])
 setwd(dir)
 require('pdq')
+require('methods')
 
 rdJobCounts = read.table("rdJobCount.log",col.names=c("rdJobCount"))
+rdJobCounts = max(rdJobCounts,rep(1,nrow(rdJobCounts)))
 wrJobCounts = read.table("wrJobCount.log",col.names=c("wrJobCount"))
+wrJobCounts = max(wrJobCounts,rep(1,nrow(wrJobCounts)))
 rdServTimes = read.table("rdServTime.log",col.names=c("rdServTime"))
 wrServTimes = read.table("wrServTime.log",col.names=c("wrServTime"))
 
@@ -16,26 +19,29 @@ rdNums = read.table("rdNum.log",col.names=c("rdNum"))
 wrNums = read.table("wrNum.log",col.names=c("wrNum"))
 
 rdThinkTimes = (rdJobCounts*Interval - rdNums * rdAccessTimes)/rdNums
+colnames(rdThinkTimes)=c("rdThinkTimes")
 wrThinkTimes = (wrJobCounts*Interval - wrNums * wrAccessTimes)/wrNums
+colnames(wrThinkTimes)=c("wrThinkTimes")
 
-interval_data=data.frame(rdJobCounts,wrJobCounts,rdThinkTimes,wrThinkTimes,rdServTimes,wrServTimes)
+interval_data=data.frame(rdJobCounts,wrJobCounts,rdServTimes,wrServTimes,rdThinkTimes,wrThinkTimes)
 
 analyze_interval <- function( params ){
 	rdJobCount <- params[1][[1]]
 	wrJobCount <- params[2][[1]]
-	rdThinkTime <- params[3][[1]]
-	wrThinkTime <- params[4][[1]]
-	rdServTime <- params[5][[1]]
-	wrServTime <- params[6][[1]]
+	rdServTime <- params[3][[1]]
+	wrServTime <- params[4][[1]]
+	rdThinkTime <- params[5][[1]]
+	wrThinkTime <- params[6][[1]]
 	Init("dram queue")
-	CreateClosed("read",TERM,rdJobCount,as.numeric(rdThinkTime))
-	CreateClosed("write",TERM,wrJobCount,as.numeric(wrThinkTime))
 	CreateNode("dram",CEN,FCFS)
+	CreateClosed("read",TERM,max(rdJobCount,1),rdThinkTime)
 	SetDemand("dram","read",rdServTime)
+	CreateClosed("write",TERM,max(wrJobCount,1),wrThinkTime)
 	SetDemand("dram","write",wrServTime)
 	Solve(EXACT)
 	res <- c(GetResponse(TERM,"read"),GetResponse(TERM,"write"),GetQueueLength("dram","read",TERM),GetQueueLength("dram","write",TERM),GetThruput(TERM,"read"),GetThruput(TERM,"write"),GetUtilization("dram","read",TERM),GetUtilization("dram","write",TERM))
 	return(res)
+
 }
 
 rdRespTimes <- vector("list",nrow(interval_data))
@@ -53,10 +59,10 @@ for (index in 1:nrow(interval_data)){
 	wrRespTimes[[index]] <- res[2]
 	rdQOccs[[index]] <- res[3]
 	wrQOccs[[index]] <- res[4]
-        rdThrus[[index]] <- res[5]
-        wrThrus[[index]] <- res[6]
-        rdUtils[[index]] <- res[7]
-        wrUtils[[index]] <- res[8]
+  rdThrus[[index]] <- res[5]
+  wrThrus[[index]] <- res[6]
+  rdUtils[[index]] <- res[7]
+  wrUtils[[index]] <- res[8]
 }
 rdRespTimes <- data.frame(unlist(rdRespTimes))
 wrRespTimes <- data.frame(unlist(wrRespTimes))
@@ -69,3 +75,7 @@ wrUtils <- data.frame(unlist(wrUtils))
 
 perf_params <- data.frame(rdThrus,wrThrus,rdUtils,wrUtils,rdQOccs,wrQOccs,rdRespTimes,wrRespTimes)
 colnames(perf_params) <- c("pdq_rdThru","pdq_wrThru","pdq_rdUtil","pdq_wrUtil","pdq_rdQOcc","pdq_wrQOcc","pdq_rdResp","pdq_wrResp")
+print(perf_params)
+
+rdRespTimesCmp=data.frame(rdRespTimes,rdAccessTimes)
+wrRespTimesCmp=data.frame(wrRespTimes,wrAccessTimes)
